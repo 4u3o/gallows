6a513fd7c27bf3869f3e305ffedd0f832e06d49a
Anton Goroshko <radjahair@gmail.com>
2020-10-06 20:00:41 +1000

    start gallows project

---
+++ lib/game.rb
@@ -0,0 +1,59 @@
+class Game
+  TOTAL_ERRORS_ALLOWED = 7
+
+  def initialize(word)
+    @letters = word.chars
+    @user_guesses = []
+  end
+
+  def errors
+    @user_guesses - normalized_letters
+  end
+
+  def errors_made
+    errors.length
+  end
+
+  def errors_allowed
+    TOTAL_ERRORS_ALLOWED - errors_made
+  end
+
+  def letters_to_guess
+    @letters.map do |letter|
+      @user_guesses.include?(normalize_letter(letter)) ? letter : nil
+    end
+  end
+
+  def lost?
+    errors_allowed.zero?
+  end
+
+  def normalize_letter(letter)
+    return 'И' if letter == 'Й'
+    return 'Е' if letter == 'Ё'
+
+    letter
+  end
+
+  def normalized_letters
+    @letters.map(&method(:normalize_letter))
+  end
+
+  def over?
+    won? || lost?
+  end
+
+  def play!(letter)
+    unless over? || @user_guesses.include?(normalize_letter(letter))
+      @user_guesses << normalize_letter(letter)
+    end
+  end
+
+  def won?
+    (normalized_letters - @user_guesses).empty?
+  end
+
+  def word
+    @letters.join
+  end
+end
